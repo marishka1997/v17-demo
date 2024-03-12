68951c7b2f7bef3f24210c8c0da055e404ee21a9
Mariami Gaspariani <mgaspariani@dga.gov.ge>
2024-03-12 18:11:23 +0400

    feat: manipulating tasks with signals - demo

--- src/app/manipulating-tasks-signals/manipulating-tasks-signals.component.ts
+++ src/app/manipulating-tasks-signals/manipulating-tasks-signals.component.ts
@@ -1,12 +1,73 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+const DEFAULT_TASKS = [
+  'Bug Fix',
+  'Code Refactoring',
+  'Feature Implementation',
+  'Backlog Refinement',
+  'Daily Meeting',
+];
 
 @Component({
   selector: 'app-manipulating-tasks-signals',
   standalone: true,
-  imports: [],
-  templateUrl: './manipulating-tasks-signals.component.html',
-  styleUrl: './manipulating-tasks-signals.component.scss'
+  imports: [FormsModule],
+  template: `
+    <h1>Manipulating Tasks with Signals</h1>
+
+    <form (submit)="addTask(addTaskInput.value)">
+      <input style="margin-right: 10px;" type="text" #addTaskInput />
+      <button
+        style="background-color: green; color: white; border: none; padding: 5px 10px; font-weight: bold;"
+        type="submit"
+      >
+        Add item
+      </button>
+    </form>
+
+    <div>
+      <span style="font-weight: bold;">Filter: </span>
+      <input
+        type="text"
+        #filterTask
+        [value]="filter()"
+        (input)="filter.set(filterTask.value)"
+      />
+    </div>
+    <button
+      style="background-color: red; color: white; border: none; padding: 5px 10px; font-weight: bold;"
+       (click)="reset()"
+    >
+      Reset
+    </button>
+    <h3>Filtered Items</h3>
+    <ul>
+      @for (item of filteredTasks(); track item) {
+      <li>{{ item }}</li>
+      }
+    </ul>
+  `,
 })
 export class ManipulatingTasksSignalsComponent {
+  tasks = signal([...DEFAULT_TASKS]);
+  filter = signal('');
 
+  filteredTasks = computed(() => {
+    // TODO 1 How often is the filter triggered on reset()
+    console.log(`tasks=${this.tasks()} filter=${this.filter()}`);
+    const lowerCaseFilter = this.filter().toLowerCase();
+    return this.tasks().filter((task) =>
+      task.toLowerCase().includes(lowerCaseFilter)
+    );
+  });
+
+  reset() {
+    this.tasks.set([...DEFAULT_TASKS]);
+    this.filter.set('');
+  }
+
+  addTask(task: string) {
+    this.tasks.update((tasks) => [...tasks, task]);
+  }
 }
