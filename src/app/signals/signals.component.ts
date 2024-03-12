2bf1d635bb4af0b7fe795f034e518a787582dd01
Mariami Gaspariani <mgaspariani@dga.gov.ge>
2024-03-12 17:56:28 +0400

    feat: signals demo

--- src/app/signals/signals.component.ts
+++ src/app/signals/signals.component.ts
@@ -1,12 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, computed, effect, signal } from '@angular/core';
 
 @Component({
   selector: 'app-signals',
   standalone: true,
   imports: [],
-  templateUrl: './signals.component.html',
-  styleUrl: './signals.component.scss'
+  template: `
+    <h1 style="color: darkred;">Signals</h1>
+
+    <!-- TODO 2b read signal -->
+    <p style="color: chocolate;">Counter: {{ counter() }}</p>
+    <!-- <p>Counter: 0</p> -->
+    <!-- TODO 6 read computed signal -->
+    <p style='color: chocolate;'>Multiple of 5: {{ isMultipleOf5() }}</p>
+    <!-- <p>Multiple of 5: ???</p> -->
+
+    <button
+      style="background-color: green; color: black; border: none; padding: 5px 10px; margin-right: 10px; font-weight: bold;"
+      (click)="increment()"
+    >
+      Increment
+    </button>
+    <button
+      style="background-color: green; color: black; border: none; padding: 5px 10px;  margin-right: 10px; font-weight: bold;"
+      (click)="reset()"
+    >
+      Reset
+    </button>
+    <button
+      style="background-color: yellow; color: black; border: none; padding: 5px 10px; font-weight: bold;"
+      (click)="log()"
+    >
+      Log
+    </button>
+  `,
 })
 export class SignalsComponent {
+  // TODO 1 create signal
+  counter = signal(0);
 
+  // TODO 5 computed signal
+  isMultipleOf5 = computed(() => this.counter() % 5 === 0);
+
+  constructor() {
+    // TODO 7 side effects
+    effect(() => console.log('Is multiple of 5: ' + this.isMultipleOf5()));
+  }
+
+  reset() {
+    // TODO 3 set value
+    this.counter.set(0);
+  }
+
+  increment() {
+    // TODO 4 update value
+    this.counter.update((value) => value + 1);
+  }
+
+  log() {
+    // TODO 2a read current value
+    console.log(this.counter());
+  }
 }
